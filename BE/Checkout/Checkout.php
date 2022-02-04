7c1b1002ef1205430dbe3cceccbf3a0751846810
Vu Hoang Minh <h.minh124412@gmail.com>
2022-02-04 21:20:43 +0700

    update DB and BE and IMG

---
+++ BE/Checkout/Checkout.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+require_once '../ConnectDataBase/connect.php';
+
+$today = getdate()['mday'] . '/' . getdate()['mon'] . '/' . getdate()['year'];
+
+$stmt = $conn->prepare("UPDATE `orders` SET `date` = :todate, 
+                                            `toname` = :toname, 
+                                            `toemail` = :toemail, 
+                                            `tophone` = :tophone,
+                                            `toaddress` = :toaddress, 
+                                            `payment` = :payment, 
+                                            `status` = 'paid' 
+                        WHERE `id` = :orderid AND `userid` = :userid ;");
+$stmt->bindParam('todate', $today);
+$stmt->bindParam('toname', $_POST['name']);
+$stmt->bindParam('toemail', $_POST['email']);
+$stmt->bindParam('tophone', $_POST['phone']);
+$stmt->bindParam('toaddress', $_POST['address']);
+$stmt->bindParam('payment', $_POST['payment']);
+$stmt->bindParam('orderid', $_POST['orderid']);
+$stmt->bindParam('userid', $_POST['userid']);
+
+
+$stmt->execute();
--- BE/Users/Users.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-require_once '../ConnectDataBase/connect.php';
