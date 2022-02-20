d62ea78e204ae4d2fad5187d0cb3b400cfb777c3
Nguyen Tuan Thanh <ntuanthanh184@gmail.com>
2022-02-21 06:22:41 +0700

    ok

--- BE/DataList/Related.php
+++ BE/DataList/Related.php
@@ -10,16 +10,7 @@ $sql = "SELECT `products`.*,`productimg`.`src` FROM `products`
                 WHERE `products`.`id` = `productimg`.`productid`
                   AND `products`.`brand` = `brand`.`id`
                   AND a.`anniversary` = `products`.`anniversary`
-            GROUP BY `products`.`id`
-            UNION 
-            SELECT `products`.*,`productimg`.`src` FROM `products` 
-            INNER JOIN `productimg`
-            INNER JOIN `brand`
-            INNER JOIN (SELECT `anniversary`,`brand` FROM `products` WHERE id = :id) as a
-                WHERE `products`.`id` = `productimg`.`productid`
-                  AND `products`.`brand` = `brand`.`id`
-                  AND a.`brand` = `brand`.`id`
-            GROUP BY `products`.`id`;";
+            GROUP BY `products`.`id`";
 $stmt = $conn->prepare("$sql");
 $stmt->bindParam('id', $_POST['productid']);
 $stmt->execute();
@@ -36,26 +27,26 @@ foreach ($stmt->fetchAll() as $key => $value) {
     );
 }
 
-// $sql = "SELECT `products`.*,`productimg`.`src` FROM `products` 
-//             INNER JOIN `productimg`
-//             INNER JOIN `brand`
-//             INNER JOIN (SELECT `anniversary`,`brand` FROM `products` WHERE id = :id) as a
-//                 WHERE `products`.`id` = `productimg`.`productid`
-//                   AND `products`.`brand` = `brand`.`id`
-//                   AND a.`brand` = `brand`.`id`
-//             GROUP BY `products`.`id`;";
-// $stmt = $conn->prepare("$sql");
-// $stmt->bindParam('id', $_POST['productid']);
-// $stmt->execute();
+$sql = "SELECT `products`.*,`productimg`.`src` FROM `products` 
+            INNER JOIN `productimg`
+            INNER JOIN `brand`
+            INNER JOIN (SELECT `anniversary`,`brand` FROM `products` WHERE id = :id) as a
+                WHERE `products`.`id` = `productimg`.`productid`
+                  AND `products`.`brand` = `brand`.`id`
+                  AND a.`brand` = `brand`.`id`
+            GROUP BY `products`.`id`;";
+$stmt = $conn->prepare("$sql");
+$stmt->bindParam('id', $_POST['productid']);
+$stmt->execute();
 
-// foreach ($stmt->fetchAll() as $key => $value) {
-//     $data[] = array(
-//         'id' => $value['id'],
-//         'name' => $value['name'],
-//         'price' => $value['price'],
-//         'src' => $value['src'],
-//         'sale' => $value['sale']
-//     );
-// }
+foreach ($stmt->fetchAll() as $key => $value) {
+    $data[] = array(
+        'id' => $value['id'],
+        'name' => $value['name'],
+        'price' => $value['price'],
+        'src' => $value['src'],
+        'sale' => $value['sale']
+    );
+}
 
 die(json_encode($data));
